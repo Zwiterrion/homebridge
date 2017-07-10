e9944f5eb39d248d466382ffd8082c23e61d3f43
fnadaud <nadauflo@gmail.com>
2017-07-10 13:34:42 +0200

    2 lampes et 2 verrous dans l'app

--- react-app/src/components/accessory-app.js
+++ react-app/src/components/accessory-app.js
@@ -7,26 +7,26 @@ const AccessoryApp = React.createClass({
   render () {
     return (
       <div className="grid">
-          <div className="1/2 grid__cell">
-            <LightBulb name="Light bulb 1"
-                id={1}
-            />
-          </div>
-          <div className="1/2 grid__cell">
-            <LightBulb name="Light bulb 2"
-                id={2}
-            />
-          </div>
-          <div className="1/2 grid__cell">
-            <Lock name="Lock 1"
-                id = {1}
-            />
-          </div>
-          <div className="1/2 grid__cell">
-            <LightBulb name="Light bulb 4"
-                id={3}
-            />
-          </div>
+        <div className="1/2 grid__cell">
+          <LightBulb name="Lampe salon"
+            id={1}
+          />
+        </div>
+        <div className="1/2 grid__cell">
+          <Lock name="Verrou entrée"
+            id = {1}
+          />
+        </div>
+        <div className="1/2 grid__cell">
+          <Lock name="Verrou coffre fort"
+            id={2}
+          />
+        </div>
+        <div className="1/2 grid__cell">
+          <LightBulb name="Lampe cuisine"
+            id={2}
+          />
+        </div>
       </div>
     );
   }
