dcbda5b92a6173f029ebdb098c296d124cafdd6c
Thybot <thibault.herault@gmail.com>
2017-08-04 16:03:37 +0200

    robotCore correction

--- robot/robotCore/src/domo/openHab/speakers.js
+++ robot/robotCore/src/domo/openHab/speakers.js
@@ -1,39 +1,39 @@
 const fetch = require('node-fetch')
 
 const DEFAULT_STATE = {
-  control : "PAUSE",
-  currentTitle : "",
-  volume : 0
+	control : "PAUSE",
+	currentTitle : "",
+	volume : 0
 }
 
 function OHSpeakers(baseURL, speakerName, state = DEFAULT_STATE){
-  this.baseURL = baseURL;
-  this.speakerName = speakerName;
-  this.state = state;
+	this.baseURL = baseURL;
+	this.speakerName = speakerName;
+	this.state = state;
 
-  const that = this;
+	const that = this;
 
-  // private function
-  function fetchPost(itemSuffixe, body){
-    return fetch(`${that.baseURL}/items/${that.speakerName}${itemSuffixe}`, {
-      method : 'POST',
-      headers : { 'content-type':'text/plain' },
-      body : body
-    }).then( result => console.log(result) )
-    .catch( error => console.log(`error : ${error}`) )
-  }
+	// private function
+	function fetchPost(itemSuffixe, body){
+		return fetch(`${that.baseURL}/items/${that.speakerName}${itemSuffixe}`, {
+			method : 'POST',
+			headers : { 'content-type':'text/plain' },
+			body : body
+		}).then( result => console.log(result) )
+			.catch( error => console.log(`error : ${error}`) )
+	}
 
-  this.play = function(){
-    return fetchPost(`_control`, `PLAY`);
-  }
+	this.play = function(){
+		return fetchPost(`_control`, `PLAY`);
+	}
 
-  this.pause = function(){
-    return fetchPost(`_control`,`PAUSE`);
-  }
+	this.pause = function(){
+		return fetchPost(`_control`,`PAUSE`);
+	}
 
-  this.setVolume = function(volume){
-    return fetchPost(`_volume`,`${volume}`);
-  }
+	this.setVolume = function(volume){
+		return fetchPost(`_volume`,`${volume}`);
+	}
 
 }
 
