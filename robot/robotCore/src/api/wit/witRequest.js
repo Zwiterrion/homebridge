9e5c98b5c6b4cf8a06d1d72dba86c18d4a9b5414
Thybot <thibault.herault@gmail.com>
2017-08-07 09:21:14 +0200

    add witRequest and launch script for robot server

---
+++ robot/robotCore/src/api/wit/witRequest.js
@@ -0,0 +1,17 @@
+const fetch = require('node-fetch')
+
+const TOKEN = 'M6TTF3QQLPHJ7VSCSMDCFRSK2H4NNHEJ';
+const API_ENDPOINT = 'https://api.wit.ai/speech';
+
+function sendWitRequest(data){
+	return fetch(API_ENDPOINT, {
+		method: 'POST',
+		body : data,
+		headers: {
+			'authorization' : 'Bearer ' + TOKEN,
+			'Content-Type' : 'audio/wav'
+		}
+	}).then(result => {console.log(result);return result} )
+}
+
+module.exports = sendWitRequest;
