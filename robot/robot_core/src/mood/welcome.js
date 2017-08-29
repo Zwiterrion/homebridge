6aa5e7f5436ffa0ffebd43e0f88c8efd5fcce870
fnadaud <nadauflo@gmail.com>
2017-08-29 17:03:07 +0200

    move voicerss API call to the server

--- robot/robot_core/src/mood/welcome.js
+++ robot/robot_core/src/mood/welcome.js
@@ -1,5 +1,8 @@
 const { eventEmitter, events } = require('../events.js')
 const logger = require('../utils/logger.js');
+const Voicerss = require('../api/tts/voicerss.js');
+
+const voicerss = new Voicerss();
 
 function bindEvents(){
     eventEmitter.on(events.domoEvents.lampOn, () => {
@@ -7,7 +10,13 @@ function bindEvents(){
         eventEmitter.emit('sse', events.sse.voiceSynthesis, "coucou")
     })
     eventEmitter.on(events.domoEvents.lampOff, () => logger.info('A lamp has been switched off'))
-    eventEmitter.on(events.userEvents.faceRecognize, ( faces ) => logger.info(`faces recognize : ${ faces.map( (face) => face.name)}`))
+    eventEmitter.on(events.userEvents.faceRecognize, ( faces ) =>{
+        logger.info(`faces recognize : ${ faces.map( (face) => face.name)}`)
+        voicerss.textToSpeech(`Bonjour ${faces[0].name}. Quel est votre couleur préférée ?`)
+        .then(url =>
+            eventEmitter.emit('sse', events.sse.voiceSynthesis, url)
+         )
+    })  
 }
 
 module.exports = bindEvents;
